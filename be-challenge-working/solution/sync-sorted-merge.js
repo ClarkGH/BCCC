8477436091cc60e6959c06ab36f825dfd399d365
clarkgh <demolisho@hotmail.com>
2017-08-16 01:09:19 -0700

    rough and dirty solution up

--- be-challenge-working/solution/sync-sorted-merge.js
+++ be-challenge-working/solution/sync-sorted-merge.js
@@ -1,6 +1,22 @@
-'use strict'
+'use strict';
 
-module.exports = (logSources, printer) => {
-  printer.print( logSources[0].pop() );
+const _ = require('lodash');
+
+module.exports = ( logSources, printer ) => {
+  var i = 0,
+    sortedArr = [];
+
+  while ( i < logSources.length ) {
+    logSources[i].pop();     
+    if ( logSources[i].drained ) {
+      i++;
+    }
+  }
+
+  sortedArr = _.sortBy( logSources, 'last.date' );
+  sortedArr.forEach( ( el ) => {
+    printer.print( el.last );
+  });
+
   printer.done();
 }
