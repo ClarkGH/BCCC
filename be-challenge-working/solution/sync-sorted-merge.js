6bd84ce02ea08068fa84d1080331d74b785e45de
clarkgh <demolisho@hotmail.com>
2017-08-15 23:59:10 -0700

    have to go to bed, but I think I get the gist of what they want

--- be-challenge-working/solution/sync-sorted-merge.js
+++ be-challenge-working/solution/sync-sorted-merge.js
@@ -1,12 +1,6 @@
 'use strict'
 
-const _ = require('lodash');
-
 module.exports = (logSources, printer) => {
-  var newArr = [];
-
-  for ( var i = 0; i < logSources.length; i++ ) {
-    console.log( logSources[i].pop() ); 
-  }
-
+  printer.print( logSources[0].pop() );
+  printer.done();
 }
